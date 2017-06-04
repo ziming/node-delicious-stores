3498d9588223cbc0593832546c3f6d391916e27d
Ziming Lan <ziming.lan@gmail.com>
2017-06-04 22:52:43 +0800

    not sure what changes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
- const express = require('express');
+const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 const reviewController = require('../controllers/reviewController');
-const {catchErrors} = require('../handlers/errorHandlers'); // object destructuring? just take 1 function only
+const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
@@ -16,6 +16,7 @@ router.post('/add',
     catchErrors(storeController.resize),
     catchErrors(storeController.createStore)
 );
+
 router.post('/add/:id',
     storeController.upload,
     catchErrors(storeController.resize),
@@ -23,7 +24,6 @@ router.post('/add/:id',
 );
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
-
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
@@ -31,9 +31,11 @@ router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
 router.post('/login', authController.login);
-
 router.get('/register', userController.registerForm);
 
+// 1. Validate the registration data
+// 2. register the user
+// 3. we need to log them in
 router.post('/register',
     userController.validateRegister,
     userController.register,
@@ -58,12 +60,13 @@ router.post('/reviews/:id',
 );
 
 router.get('/top', catchErrors(storeController.getTopStores));
+
 /*
  API
  */
+
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
 router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
-
 
 module.exports = router;
